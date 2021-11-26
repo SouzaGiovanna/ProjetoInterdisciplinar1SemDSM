7eb1c3fe6decc186bfc6210b5e9dbbad7904df0a
Giovanna de Souza Almeida <gi.souza03@gmail.com>
2021-11-26 09:39:37 -0300

    Tela de Cadastro quase pronta
    as validações não estão funcionando tem q ver o pq

--- js/script.js
+++ js/script.js
@@ -1,10 +1,14 @@
 function validarCadastro(){
-    let nome = cadastro.nome.value;
-    let email = cadastro.email.value;
     let senha = cadastro.senha.value;
     let confirma = cadastro.confirma.value;
+    let registro = cadastro.registro_txt.value;
     
     let regex = /[A-Za-z\d@$!%*#?&]/;
+
+    if(isNaN(registro)){
+        alert("O número de Registro na Fatec precisa ser um número inteiro");
+        return false;
+    }
 
     if(regex.test(senha)){
         alert("Sua senha precisa ter pelo menos uma letra maiúscula, uma minuscula, um número e um caractere especial!");
@@ -28,7 +32,10 @@ function mudarRegistro(){
     let value = select.options[select.selectedIndex].value;
     
     if(value == "RM"){
-        alert("entrou");
-        registro.setAttribute('maxLength', 5);;
+        registro.setAttribute('maxLength', 5);
+        registro.value = "";
+    } else {
+        registro.setAttribute('maxLength', 13);
+        registro.value = "";
     }
 }
